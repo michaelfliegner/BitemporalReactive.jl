16210cb64d4982f36545f64a5519eb8d6f425640
Michael Fliegner <Michael.Fliegner@hamburg.de>
2022-09-08 08:17:40 +0200

    PartnerSectionView display ok

--- src/ContractSectionView.jl
+++ src/ContractSectionView.jl
@@ -1,6 +1,8 @@
 module ContractSectionView
 using Stipple, Stipple.Html, StippleUI
 using BitemporalPostgres, LifeInsuranceDataModel, TimeZones
+include("HistoryView.jl")
+using .HistoryView
 
 """
 ContractsModel
@@ -88,42 +90,6 @@ function product()
   )
 end
 
-
-"""
-renderhforest
-Display the tree of versions, history tab
-"""
-function renderhforest(model)
-  quasar(:tree, ref="tree", var"node-key"="label", var"children-key"="children", nodes=:histo, var"default-expand-all"=false,
-    var"selected"=:selected_version,
-    """
-    <template v-slot:default-header="prop">
-    <div class="row items-center">
-      <div v-if="prop.node.icon">
-      <q-icon :name="prop.node.icon" size="28px" class="q-mr-sm" />
-      </div>
-          <q-field color="grey-3" label-color="primary" outlined>
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="0"><b>{{prop.node.label}}</b></div>
-            </template>
-          </q-field>
-          <q-field label="valid as of" stack-label outlined >
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="4">{{prop.node.time_valid_asof}}</div>
-            </template>
-          </q-field> 
-          <q-field label="committed" stack-label outlined >
-            <template v-slot:control>
-              <div class="self-center full-width no-outline" tabindex="2">{{prop.node.time_committed}}</div>
-            </template>
-          </q-field>     
-        </div>
-    </div>
-  </template>
-  <template v-slot:default-body="prop">
-  </template>
-    """)
-end
 
 """
 tariff_item_partners
--- src/PartnerSectionView.jl
+++ src/PartnerSectionView.jl
@@ -1,15 +1,49 @@
 module PartnerSectionView
-using Stipple
+using LifeInsuranceDataModel, Stipple, StippleUI, TimeZones
+include("HistoryView.jl")
+using .HistoryView
 
 @reactive mutable struct PartnersModel <: ReactiveModel
+  partners::R{Vector{Partner}} = []
+  current_partner::R{Partner} = Partner()
+  selected_partner_idx::R{Integer} = -1
+  process::R{Bool} = false
+  selected_version::R{String} = ""
+  current_version::R{Integer} = 0
+  txn_time::R{ZonedDateTime} = now(tz"Africa/Porto-Novo")
+  ref_time::R{ZonedDateTime} = now(tz"Africa/Porto-Novo")
   ps::R{Dict{String,Any}} = Dict{String,Any}("tsdb_validfrom" => "2022-09-07T09:00:02.844+00:00", "ref_history" => Dict{String,Any}("value" => 9223372036854775807), "revision" => Dict{String,Any}("ref_validfrom" => Dict{String,Any}("value" => 1), "ref_invalidfrom" => Dict{String,Any}("value" => 9223372036854775807), "date_of_birth" => "", "ref_component" => Dict{String,Any}("value" => 0), "id" => Dict{String,Any}("value" => 0), "description" => ""), "ref_version" => Dict{String,Any}("value" => 9223372036854775807), "tsw_validfrom" => "2022-09-07T09:00:02.844+00:00")
+  tab::R{String} = "psection"
 end
 
-function ui(partnersModel::PartnersModel)
-  page(
-    partnersModel,
-    class="container",
-    [
+"""
+partner_list
+Display the contracts selection tab
+"""
+function partner_list()
+  """
+    <template v-for="(cid,cindex) in partners">
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-list dense bordered padding class="rounded-borders">
+          <q-item clickable v-ripple v-on:click="selected_partner_idx=cindex">
+            <q-item-section>
+              {{cid['id']['value']}}
+            </q-item-section>
+          </q-item>   
+        </q-list>
+      </div>
+    </template>
+  """
+end
+
+"""
+partner()
+partner version tab
+"""
+function partner()
+  card(class="my-card bg-purple-8 text-white",
+    [card_section([Html.div(class="text-h2 text-white", "Partner {{ps['revision']['ref_component']['value']}}"),
+      Html.div(class="text-h5 text-white", "valid as of {{ref_time}} transaction time {{txn_time}}"),
       """
       <q-markup-table dark class="bg-deep-purple-5 text-white">
                       <template>
@@ -31,17 +65,116 @@ function ui(partnersModel::PartnersModel)
                         </tbody>
                        </template>
                     </q-markup-table>
-      """]
-  )
+      """
+    ])
+    ])
 end
 
-# partnersModel = PartnersModel |> init
-# 
-# route("/PartnerSection") do
-# 
-#   html(ui(partnersModel), context=@__MODULE__)
-# end
-# 
-# up()
+"""
+page_content(model)
+provides the tab structure slots
+"""
+function page_content(model)
+  join([
+    """
+
+          <q-tab-panels
+            v-model="tab"
+            animated
+            swipeable
+            horizontal
+            transition-prev="jump-up"
+            transition-next="jump-up"
+          >
+            <q-tab-panel name="partners">
+                <div class="text-h4 q-mb-md">Partners</div>
+    """,
+    partner_list(),
+    """       
+            </q-tab-panel>
+            <q-tab-panel name="history">
+                <div class="text-h4 q-mb-md">History</div>
+    """,
+    renderhforest(model),
+    """ 
+            </q-tab-panel>
+            <q-tab-panel name="psection">
+    """,
+    partner(),
+  ])
+end
+
+
+"""
+ui(model)
+entry point of the vue app
+"""
+function ui(model::PartnerSectionView.PartnersModel)
+  page(
+    model,
+    class="container",
+    Genie.Renderer.Html.div(join([
+      """
+        <q-layout view="hHh lpR fFf">
+          <q-header elevated class="bg-primary text-white" >
+            <q-toolbar>
+              <!-- <q-btn dense flat icon="drawer" @click="leftDrawerOpen=!leftDrawerOpen" /></q-btn> -->
+              <q-btn color="primary" icon="menu" >
+                <q-menu>
+                  <q-list style="min-width: 100px">
+                    <q-item clickable v-close-popup>
+                      <q-item-section>New tab</q-item-section>
+                    </q-item>
+                    <q-item clickable v-close-popup>
+                      <q-item-section>New incognito tab</q-item-section>
+                    </q-item>
+                    <q-item clickable v-close-popup>
+                      <q-item-section>Recent tabs</q-item-section>
+                    </q-item>
+                    <q-item clickable v-close-popup>
+                      <q-item-section>History</q-item-section>
+                    </q-item>
+                    <q-item clickable v-close-popup>
+                      <q-item-section>Downloads</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item clickable v-close-popup>
+                      <q-item-section>Settings</q-item-section>
+                    </q-item>
+                    <q-separator />
+                    <q-item clickable v-close-popup>
+                      <q-item-section>Help &amp; Feedback</q-item-section>
+                    </q-item>
+                  </q-list>
+                </q-menu>
+              </q-btn>
+              <q-toolbar-title>
+              BitemporalReactive
+              </q-toolbar-title>
+            </q-toolbar>
+                  <q-tabs
+        v-model="tab"
+        class="bg-primary text-white shadow-2" align="left"
+      >
+        <q-tab name="partners" icon="format_list_bulleted" label="Search Partner"></q-tab>
+        <q-tab name="psection" icon="verified_user" label="Partner Version"></q-tab>
+        <q-tab name="history" icon="history" label="Partner History"></q-tab>
+      </q-tabs>
+      </q-header>
+          <!-- <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered> -->
+      """,
+      # drawer_content(),
+      """
+      <!--   </q-drawer> -->
+         <q-page-container> 
+     """,
+      page_content(model),
+      """
+          </q-page-container>
+        </q-layout>
+      """]
+    )))
+
+end
 
 end
